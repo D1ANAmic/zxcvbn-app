ce5ec9be3b3e8a8dd7b6aa9416260eff76b707f2
diana <diana.maria.schilling@gmail.com>
2022-07-27 22:15:25 +0200

    modify current page highlighting in menu

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -2,16 +2,16 @@
 <template>
   <div class="h-full basis-1/4 rounded-l-3xl bg-secondary p-10 pl-15 text-left">
     <div class="h-full flex flex-col justify-center">
-      <ul class="h-1/2 flex flex-col justify-evenly m-auto bg">
-        <li v-for="page in pages" :key="page.name" :class="{'bg-primary': page.name === getCurrentPageName}">{{ page.name }}</li>
-        <li>Bla</li>
-        <li>Bli</li>
-        <li>Blubb</li>
-        <li>Bla</li>
-        <li>Bli</li>
-        <li>Blubb</li>
-        <li>Bli</li>
-        <li>Blubb</li>
+      <ul class="h-1/2 flex flex-col justify-evenly m-auto ">
+        <li v-for="page in pages" :key="page.name" class="rounded p-2" :class="{'font-extrabold bg-primary': page.name === getCurrentPageName}">{{ page.name }}</li>
+        <li class="rounded p-2">Bla</li>
+        <li class="rounded p-2">Bli</li>
+        <li class="rounded p-2">Blubb</li>
+        <li class="rounded p-2">Bla</li>
+        <li class="rounded p-2">Bli</li>
+        <li class="rounded p-2">Blubb</li>
+        <li class="rounded p-2">Bli</li>
+        <li class="rounded p-2">Blubb</li>
       </ul>
     </div>
   </div>
