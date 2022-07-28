85eca5abc3d6536a4a73777c8ce4f5a59da3c702
diana <diana.maria.schilling@gmail.com>
2022-07-28 14:09:33 +0200

    modify page card style

--- src/components/cards/PageCard.vue
+++ src/components/cards/PageCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="m-auto flex h-80vh w-80vw flex-row flex-nowrap rounded-3xl bg-primary pr-3 font-poppins text-font-dark"
+    class="m-auto flex h-80vh w-80vw flex-row flex-nowrap rounded-3xl bg-primary pr-3 font-openSans text-font-dark text-left drop-shadow-2xl"
   >
     <Menu />
     <div class="flex h-full basis-3/4 flex-col overflow-y-auto p-10">
