a5171ce9f36b38e2ef1db43b2979cb98c46d7815
diana <diana.maria.schilling@gmail.com>
2022-07-28 12:59:19 +0200

    modify password validation

--- src/utils/validatePassword.js
+++ src/utils/validatePassword.js
@@ -1,33 +1,47 @@
 export const validatePassword = (password = '', ref) => {
-
-	if ( password !== '') {
-		if (password.length < 6) {
+	if (password !== '') {
+		if (password.length < 8) {
 			ref.focus();
 
-			return 'Error: Password must contain at least six characters!';
+			return 'Password must contain at least 8 characters. Please try again!';
 		}
 		let re = /[0-9]/;
 		if (! re.test(password)) {
 			ref.focus();
 
-			return 'Error: password must contain at least one number (0-9)!';
+			return 'Password must contain at least 1 number (0-9). Please try again!';
 		}
 		re = /[a-z]/;
 		if (! re.test(password)) {
 			ref.focus();
 
-			return 'Error: password must contain at least one lowercase letter (a-z)!';
+			return 'Password must contain at least 1 lowercase letter (a-z). Please try again!';
 		}
 		re = /[A-Z]/;
 		if (! re.test(password)) {
 			ref.focus();
 
-			return 'Error: password must contain at least one uppercase letter (A-Z)!';
+			return 'Password must contain at least 1 uppercase letter (A-Z)!';
+		}
+		re = /[!@#$%^&*_]/;
+		if (! re.test(password)) {
+			ref.focus();
+
+			return 'Password must contain at least 1 special character. Good luck finding out which ones are allowed!';
+		}
+		re = /^[^\s]*(\s[^\s]*){3}$/;
+		if (! re.test(password)) {
+			ref.focus();
+
+			return 'Password must contain exactly 3 spaces! (Dear attackers, please pretend you never read that.)';
+		}
+		else {
+			return 'Way to go - you\'re almost there! Password must contain at least 1 hieroglyph, 1 Klingon numeral, your firstborn and an emoji. Please try again!';
 		}
 	}
 	else {
 		ref.focus();
 
-		return 'Error: Please check that you\'ve entered and confirmed your password!';
+		return 'Looks like you haven\'t entered a password at all.';
 	}
 };
