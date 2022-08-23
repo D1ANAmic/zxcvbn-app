49b8e7653d8328e752cedb7610b34bf91ad41666
diana <diana.maria.schilling@gmail.com>
2022-08-23 18:26:13 +0200

    improve animation handling MatchingView.vue

--- src/views/pages/MatchingView.vue
+++ src/views/pages/MatchingView.vue
@@ -16,13 +16,20 @@
           <span v-for="(value, key, index) in firstTwentyWordlist"
           :key="index"
           :id="value"
-          class="transition duration-500 linear">{{value}}, </span>
+          class="token transition duration-500 linear">{{value}}, </span>
           <span>...</span>
           </div>
         </div>
         </em>
-        <Button :handle-click="startTokensAnimation"
-        :text="'Start'"/>
+        <Button :handle-click="handleTokenAnimationButton"
+                :is-disabled="animationButtons.tokensAnimationRunning">
+          <template #icon>
+            <span
+                v-html='`<i class="fa-solid fa-play"></i>`'></span>
+            <span> Play</span>
+          </template>
+
+        </Button>
         </div>
 
         <div class="mb-20">
@@ -38,37 +45,44 @@
               <tr
                   class=" border-b dark:border-font-dark "
               >
-                <td class="sequence1">1</td>
-                <td class="sequence1 sequence2">2</td>
-                <td class="sequence2">3</td>
+                <td class="transition duration-500 linear sequence1">1</td>
+                <td class="transition duration-500 linear sequence1 sequence2">2</td>
+                <td class="transition duration-500 linear sequence2">3</td>
                 <td>s</td>
-                <td class="sequence3">8</td>
-                <td class="sequence3 sequence4">6</td>
-                <td class="sequence4">4</td>
+                <td class="transition duration-500 linear sequence3">8</td>
+                <td class="transition duration-500 linear sequence3 sequence4">6</td>
+                <td class="transition duration-500 linear sequence4">4</td>
               </tr>
               <tr>
-                <td class="sequence1">U+0031</td>
-                <td class="sequence1 sequence1">U+0032</td>
-                <td class="sequence1">U+0033</td>
+                <td class="transition duration-500 linear sequence1">U+0031</td>
+                <td class="transition duration-500 linear sequence1 sequence2">U+0032</td>
+                <td class="transition duration-500 linear sequence2">U+0033</td>
                 <td>U+0073</td>
-                <td class="sequence3">U+0038</td>
-                <td class="sequence3 sequence4">U+0036</td>
-                <td class="sequence4">U+0034</td>
+                <td class="transition duration-500 linear sequence3">U+0038</td>
+                <td class="transition duration-500 linear sequence3 sequence4">U+0036</td>
+                <td class="transition duration-500 linear sequence4">U+0034</td>
               </tr>
               <tr class="text-right">
-                <td class="sequence1">1</td>
-                <td class="sequence2">1</td>
+                <td class="transition duration-500 linear sequence1">1</td>
+                <td class="transition duration-500 linear sequence2">1</td>
                 <td>40</td>
                 <td>-35</td>
-                <td class="sequence3">-2</td>
-                <td class="sequence4">-2</td>
+                <td class="transition duration-500 linear sequence3">-2</td>
+                <td class="transition duration-500 linear sequence4">-2</td>
               </tr>
               </tbody>
             </table>
           </div>
         </em>
-        <Button :handle-click="startSequenceAnimation"
-                :text="'Start'"/>
+          <Button :handle-click="handleSequenceAnimationButton"
+                  :is-disabled="animationButtons.sequenceAnimationRunning">
+            <template #icon>
+            <span
+                v-html='`<i class="fa-solid fa-play"></i>`'></span>
+              <span> Play</span>
+            </template>
+
+          </Button>
         </div>
       </main>
     </template>
@@ -88,13 +102,30 @@ export default {
 	},
 	data() {
 		return {
+			animationButtons: {
+				tokensAnimationRunning: false,
+				sequenceAnimationRunning: false
+			},
 			firstTwentyWordlist: '123456,password,12345678,qwerty,123456789,12345,1234,111111,1234567,dragon,123123,baseball,abc123,football,monkey,letmein,shadow,master,696969,mustang'.split(','),
 			tokenTransformed: false,
 			sequenceClasses: ['sequence1', 'sequence2', 'sequence3', 'sequence4']
 		};
+
+	},
+	computed: {
 	},
 	methods: {
+		handleTokenAnimationButton() {
+			if (this.animationButtons.tokensAnimationRunning) {
+				return;
+			}
+			this.startTokensAnimation();
+		},
 		startTokensAnimation() {
+
+			this.animationButtons.tokensAnimationRunning = true;
+			this.resetTokensAnimation();
+
 			//transform to lowercase
 			const token = document.getElementById('token');
 			const tokenTransformed = document.getElementById('token-transformed');
@@ -119,6 +150,10 @@ export default {
 				setTimeout(() => {
 
 					this.toggleHighlight(wordlist, i);
+					if (i === wordlist.length - 1){
+						this.animationButtons.tokensAnimationRunning = false;
+						this.toggleTokenTransformed();
+					}
 				}, timeOut);
 			}
 
@@ -126,27 +161,56 @@ export default {
 		toggleHighlight(arr, index) {
 			arr[index].classList.add('bg-third');
 			if (index > 0) {
-				arr[index - 1].classList.remove( 'bg-third');
+				arr[index - 1].classList.remove('bg-third');
 			}
 		},
-		toggleTokenTransformed(){
+		toggleTokenTransformed() {
 			this.tokenTransformed = ! this.tokenTransformed;
 		},
-		startSequenceAnimation(){
+		resetTokensAnimation() {
+			if (this.tokenTransformed) {
+				this.toggleTokenTransformed();
+			}
+			const tokenList = document.getElementsByClassName('token');
+			Object.keys(tokenList).forEach((token) => {
+				tokenList[token].classList.remove('bg-third');
+			});
+		},
+
+		handleSequenceAnimationButton() {
+			if (this.animationButtons.sequenceAnimationRunning) {
+				return;
+			}
+			this.startSequenceAnimation();
+		},
+		startSequenceAnimation() {
+
+			this.animationButtons.sequenceAnimationRunning = true;
+			this.resetSequenceAnimation();
 
 			for (let i = 0; i < this.sequenceClasses.length; i ++) {
-				console.log('here');
-				const sequenceElements = document.getElementsByClassName('sequence' + i + 1);
-				console.log(sequenceElements);
-				const timeOut = 400 * i + 1500;
+				const sequenceElements = document.getElementsByClassName('sequence' + (i + 1));
+				const timeOutAdd = 1000 * (i + 1);
+				const timeOutRemove = timeOutAdd + 500;
 
 				setTimeout(() => {
 
-					sequenceElements.forEach((element) => {
-						element.classList.add('bg-third');
+					Object.keys(sequenceElements).forEach((element) => {
+						sequenceElements[element].classList.add('text-third');
+
 					});
-				}, timeOut);
+
+					if (i === this.sequenceClasses.length - 1){
+						this.animationButtons.sequenceAnimationRunning = false;
+					}
+				}, timeOutAdd);
 			}
+		},
+		resetSequenceAnimation() {
+			const sequenceElements = document.getElementsByTagName('td');
+			Object.keys(sequenceElements).forEach((element) => {
+				sequenceElements[element].classList.remove('text-third');
+			});
 		}
 	}
 };
