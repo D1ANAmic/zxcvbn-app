2855626be3378844fcba7c5abe6486476105ebf3
diana <diana.maria.schilling@gmail.com>
2022-08-25 09:20:45 +0200

    add ConclusionView.vue basic structure

---
+++ src/views/pages/ConclusionView.vue
@@ -0,0 +1,29 @@
+<template>
+  <PageCard :header="'Is Zxcvbn the solution to all our problems?'">
+    <template>
+      <main>
+        <Paragraph
+            paragraph="Bla."
+        ></Paragraph>
+      </main>
+    </template>
+  </PageCard>
+</template>
+
+<script>
+import Paragraph from '@/components/text/Paragraph';
+import PageCard from '@/components/cards/PageCard';
+
+export default {
+	components: {
+		Paragraph,
+		PageCard
+	},
+	data() {
+		return {
+			value: ''
+		};
+	}
+
+};
+</script>
