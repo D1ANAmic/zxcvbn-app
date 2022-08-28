86151be9cacedd06ff2bc0d86de79c99ba687661
diana <diana.maria.schilling@gmail.com>
2022-08-28 20:30:49 +0200

    fix menu styling and navigation error

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="h-full basis-1/4 rounded-l-3xl bg-secondary p-10 pl-15 text-left text-third">
+  <div class="h-full basis-1/2 max-w-[30%] min-w-[30%] rounded-l-3xl bg-secondary p-10 pl-15 text-left text-third">
     <div class="h-full flex flex-col justify-center">
       <ul class="h-1/2 flex flex-col justify-evenly m-auto ">
         <li v-for="page in pages" :key="page.name" class="rounded p-2 cursor-pointer tracking-wide" :class="{' menu-active font-extrabold tracking-normal': page.name === getCurrentPageName}"
@@ -31,6 +31,9 @@ export default {
 	methods: {
 		getPageLink(page) {
 			const { pageNumber, path } = page;
+			if (pageNumber === this.currentPage){
+				return;
+			}
 			this.$store.commit('SET_CURRENT_PAGE', pageNumber);
 			this.$router.push(path);
 		}
