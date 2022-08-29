52afbb2e92b00f2326ff480a56a0aaacb3c41c5a
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:19:53 +0200

    refactor Menu.vue

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,11 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="h-full basis-1/2 max-w-[30%] min-w-[30%] rounded-l-3xl bg-secondary p-10 pl-15 text-left text-third">
-    <div class="h-full flex flex-col justify-center">
-      <ul class="h-1/2 flex flex-col justify-evenly m-auto ">
-        <li v-for="page in pages" :key="page.name" class="rounded p-2 cursor-pointer tracking-wide" :class="{' menu-active font-extrabold tracking-normal': page.name === getCurrentPageName}"
-          @click="getPageLink(page)" @keyup.enter="getPageLink(page)">
-          <a>{{ page.name }}</a>
+  <div
+    class="pl-15 h-full min-w-[30%] max-w-[30%] basis-1/2 bg-secondary p-10 text-left font-prozaLibre text-font-light"
+  >
+    <div class="flex h-full flex-col justify-center">
+      <ul class="m-auto flex h-1/2 flex-col justify-evenly">
+        <li
+          v-for="(page, index) in pages"
+          :key="page.name"
+          :index="index"
+          class="cursor-pointer rounded p-2 tracking-wide"
+          :class="{
+            ' menu-active font-extrabold tracking-normal':
+              isActivePage(page.name),
+          }"
+          @click="getPageLink(page)"
+          @keyup.enter="getPageLink(page)"
+        >
+          <a>{{ index + 1 }}. {{ page.name }}</a>
         </li>
       </ul>
     </div>
@@ -16,12 +28,6 @@
 import { mapState } from 'vuex';
 
 export default {
-	components:{
-	},
-	data() {
-		return {
-		};
-	},
 	computed: {
 		...mapState(['currentPage', 'pages']),
 		getCurrentPageName() {
@@ -29,24 +35,23 @@ export default {
 		}
 	},
 	methods: {
+		isActivePage(name){
+			return this.getCurrentPageName === name;
+		},
 		getPageLink(page) {
 			const { pageNumber, path } = page;
-			if (pageNumber === this.currentPage){
+			if (pageNumber === this.currentPage) {
 				return;
 			}
 			this.$store.commit('SET_CURRENT_PAGE', pageNumber);
 			this.$router.push(path);
 		}
-	},
-	created() {
 	}
 };
 </script>
-
 
 <style lang="scss">
 .menu-active {
   text-shadow: 0 0 10px;
 }
-
 </style>
