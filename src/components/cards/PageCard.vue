c196f318dc4e44bd8dea93c18c084911591848d5
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:20:01 +0200

    refactor PageCard.vue

--- src/components/cards/PageCard.vue
+++ src/components/cards/PageCard.vue
@@ -1,31 +1,28 @@
 <template>
   <CardBackground>
-<!--    <template #menu>
-    <Menu />
-    </template>-->
     <template #card-content>
-    <div class="flex h-full basis-3/4 flex-col">
-      <header>
-        <h1 class="mt-0 mb-2 text-5xl font-medium leading-tight">
-          {{ header }}
-        </h1>
-      </header>
-      <slot></slot>
-      <Pagination />
-    </div>
+      <div class="flex h-full basis-3/4 flex-col">
+        <header>
+          <h1
+            class="mt-0 mb-2 font-prozaLibre text-5xl font-bold leading-tight text-third"
+          >
+            {{ header }}
+          </h1>
+        </header>
+        <slot></slot>
+        <Pagination />
+      </div>
     </template>
   </CardBackground>
 </template>
 
 <script>
 import CardBackground from '@/components/cards/CardBackground';
-import Menu from '@/components/menu/Menu';
 import Pagination from '@/components/pagination/Pagination';
 
 export default {
 	components: {
 		CardBackground,
-		/*Menu,*/
 		Pagination
 	},
 	props: {
