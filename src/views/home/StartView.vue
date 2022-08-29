839c84de385f29b3811a21004816032cc6028941
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:21:56 +0200

    refactor StartView.vue

--- src/views/home/StartView.vue
+++ src/views/home/StartView.vue
@@ -1,23 +1,33 @@
 <template>
   <CardBackground>
     <template #start>
-      <div class=" w-full h-full flex flex-col justify-center items-center p-20">
+      <div
+        class="flex h-full w-full flex-col items-center justify-center overflow-hidden p-5 text-xl"
+      >
         <div class="flex">
-          <img src="../../assets/images/hero3.svg" class="basis-1 min-w-[30%] max-w-[30%]">
-          <h1 class="m-auto text-5xl font-extrabold">Password Strength with Zxcvbn for Dummies</h1>
+          <img
+            src="../../assets/images/hero3.svg"
+            class="min-w-[35%] max-w-[35%] basis-1"
+          />
+          <h1 class="m-auto ml-[-20px] font-prozaLibre text-6xl font-extrabold">
+            Password Strength with Zxcvbn for Dummies
+          </h1>
         </div>
-        <Paragraph :paragraph="'Have you always wanted to know what determines a strong password? Or are you simply fed up with overly complex password policies? Then your\'re at the right place. This interactive app will provide you with the tools to compose strong passwords. '" :class="'text-center'"></Paragraph>
+        <Paragraph
+          :paragraph="'Have you always wanted to know what determines a strong password? Or are you simply fed up with overly complex password policies? Then your\'re at the right place. This interactive app will provide you with the tools to compose strong passwords. '"
+          :class="'text-center text-xl '"
+        ></Paragraph>
         <div>
-        <router-link to="/password-policies"><Button :text="'Start App'" :handle-click="increment"/></router-link>
+          <router-link to="/password-policies"
+            ><Button :text="'Start App'" :handle-click="increment"
+          /></router-link>
         </div>
       </div>
     </template>
-
   </CardBackground>
 </template>
 
 <script>
-
 import CardBackground from '@/components/cards/CardBackground';
 import { mapState } from 'vuex';
 import Button from '@/components/button/Button';
@@ -28,14 +38,6 @@ export default {
 		Paragraph,
 		Button,
 		CardBackground
-
-	},
-	props: {
-	},
-	data() {
-		return {
-			catImage: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/VAN_CAT.png'
-		};
 	},
 	methods: {
 		increment() {
@@ -46,15 +48,10 @@ export default {
 		}
 	},
 	computed: {
-		...mapState([
-			'modalOpen'
-		])
-
-
+		...mapState(['modalOpen'])
 	},
 	mounted() {
 		this.resetPageNumber();
 	}
 };
 </script>
-
