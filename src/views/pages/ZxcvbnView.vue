f99bcf3cfb877adb06e2049e8eda613943433c6a
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:22:48 +0200

    refactor ZxcvbnView.vue

--- src/views/pages/ZxcvbnView.vue
+++ src/views/pages/ZxcvbnView.vue
@@ -2,26 +2,22 @@
   <PageCard :header="'What is Zxcvbn?'">
     <template>
       <main>
-        <Paragraph
-          paragraph="As we've seen before, creating a strong password isn't easy at all - especially if we take into consideration that the password should preferably be easy to remember too. Given the vast amount of passwords we're confronted with in modern times, it is understandable why we gravitate towards simple and easily memorable passwords and sometimes even reusable. That said, usability plays a huge part in password composition but is widely disregard by common password policies. "
-        ></Paragraph>
-        <Paragraph
-          paragraph="Zxcvbn on the other hand follows a different approach. The password strength estimator was developed by <a href='https://blog.dropbox.com/authors/dan-wheeler' target='_blank' class='underline'>Dan Wheeler</a> and works with a more feasible estimation."
-        ></Paragraph>
-        <Paragraph paragraph="See for yourself:"></Paragraph>
+        <Paragraph :paragraph="'Zxcvbn is a password strength estimator developed by <a href=\'https://blog.dropbox.com/authors/dan-wheeler\' target=\'_blank\' class=\'underline\'>Dan Wheeler</a>. It is built on the same concepts attackers use and utilizes them into rules for password strength estimation.'"/>
+        <Paragraph :paragraph="'The small Zxcvbn library can easily be embedded and will calculate the number of guesses, time to crack and the overall password score for a given password based on certain patterns.'"/>
+        <Paragraph :paragraph="'See for yourself:'"/>
         <div>
           <div class="mb-4 mt-5">
-            <label
+<!--            <label
               class="mb-2 block text-sm font-bold text-gray-700"
               for="password"
             >
               Password
-            </label>
+            </label>-->
             <input
               class="focus:shadow-outline appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
               id="password"
               type="text"
-              placeholder="zxcvbn"
+              placeholder="Type in your password"
               v-model="value"
             />
           </div>
@@ -29,26 +25,26 @@
             <PasswordResults :zxcvbnObject="zxcvbnObject" />
           </div>
         </div>
-        <Paragraph paragraph="Fair enough. But how does Zxcvbn actually work? The algorithm can be devided in three essential parts:"></Paragraph>
+        <Paragraph :paragraph="'Fair enough. But how does Zxcvbn actually work? The algorithm can be divided in three essential parts:'"/>
         <em>
-        <ul>
+        <ul class="list-disc pl-10">
           <li>Matching</li>
           <li>Estimating</li>
           <li>Searching</li>
         </ul>
         </em>
-        <Paragraph paragraph="Let's explore them in the following sections."></Paragraph>
+        <Paragraph :paragraph="'Let\'s explore them in the following sections.'"/>
       </main>
     </template>
   </PageCard>
 </template>
 
 <script>
-import Paragraph from '@/components/text/Paragraph';
 const zxcvbn = require('zxcvbn');
+import Paragraph from '@/components/text/Paragraph';
 import PasswordResults from '@/components/passwordResults/PasswordResults';
-import { mapState } from 'vuex';
 import PageCard from '@/components/cards/PageCard';
+import { mapState } from 'vuex';
 
 export default {
 	components: {
