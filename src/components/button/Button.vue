7fb40092ea0d0dfdbac09a20d3e4d79f01fbe1eb
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:17:29 +0200

    refactor Button.vue

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,13 +1,14 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <button
-      @click="handleClick()"
-      type="button"
-      :disabled="isDisabled"
-      :class="getButtonClasses">
-      <slot name="icon"></slot>
-      {{ text }}
-    </button>
+  <button
+    @click="handleClick()"
+    type="button"
+    :disabled="isDisabled"
+    :class="getButtonClasses"
+  >
+    <slot name="icon"></slot>
+    {{ text }}
+  </button>
 </template>
 
 <script>
@@ -16,12 +17,14 @@ export default {
 		text: { type: String, default: '' },
 		classes: { type: String, default: '' },
 		handleClick: { type: Function, default: () => null },
-		isDisabled: { type: Boolean, default:false }
+		isDisabled: { type: Boolean, default: false }
 	},
 	data() {
 		return {
-			defaultActiveButtonClasses: 'px-4 py-2 border-[1px] border-secondary bg-secondary text-font-light transition delay-75 duration-500 ease-in-out hover:bg-third hover:text-secondary',
-			defaultDisabledButtonClasses: 'px-4 py-2 border-[1px] border-secondary bg-secondary text-font-light opacity-50'
+			defaultActiveButtonClasses:
+        'px-4 py-2 border-[1px] border-secondary bg-secondary text-font-light transition delay-75 duration-500 ease-in-out hover:bg-third uppercase',
+			defaultDisabledButtonClasses:
+        'px-4 py-2 border-[1px] border-secondary bg-secondary text-font-light opacity-50 uppercase'
 		};
 	},
 	computed: {
@@ -29,13 +32,12 @@ export default {
 			if (this.classes && this.classes.length > 0) {
 				return this.classes;
 			}
-			if (this.isDisabled){
+			if (this.isDisabled) {
 				return this.defaultDisabledButtonClasses;
 			}
 
 			return this.defaultActiveButtonClasses;
 		}
-	},
-	mounted() {}
+	}
 };
 </script>
