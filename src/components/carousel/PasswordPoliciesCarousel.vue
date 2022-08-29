7afc51da142f6f1c00c61c88989493c1658ea393
diana <diana.maria.schilling@gmail.com>
2022-08-29 22:20:42 +0200

    refactor PasswordPoliciesCarousel.vue

--- src/components/carousel/PasswordPoliciesCarousel.vue
+++ src/components/carousel/PasswordPoliciesCarousel.vue
@@ -5,12 +5,11 @@
     <div class="relative h-72 overflow-hidden rounded-lg">
       <!-- Carousel Items -->
       <div
-        v-for="(item, index) in imageSources"
+        v-for="(_, index) in imageSources"
         :key="index"
         :id="`carousel-item-${index}`"
         data-carousel-item="active"
         class="absolute top-1/2 left-1/2 block h-full w-full -translate-x-1/2 -translate-y-1/2 bg-font-light transition-opacity duration-1000 ease-in-out flex"
-        alt="..."
         :class="index !== getCurrentElement ? 'opacity-0' : 'opacity-1'"
       >
         <img :src="imageSources[index]" class="m-auto w-3/4">
@@ -18,10 +17,10 @@
     </div>
     <!-- Slider indicators -->
     <div
-      class="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3"
+      class="absolute bottom-10 left-1/2 z-30 flex -translate-x-1/2 space-x-3"
     >
       <div
-        v-for="(item, index) in imageSources"
+        v-for="(_, index) in imageSources"
         :key="index"
         :id="`carousel-indicator-${index}`"
         class="h-3 w-3 cursor-pointer rounded-full bg-font-dark bg-opacity-20 transition delay-75 duration-500 ease-in-out hover:bg-opacity-50"
@@ -29,38 +28,43 @@
       ></div>
     </div>
     <!-- Slider controls -->
-    <button
-      @click="onPrev()"
-      id="data-carousel-prev"
-      type="button"
-      class="absolute top-0 left-0 z-30 flex h-full cursor-pointer items-center justify-center px-4"
+    <Button
+        :handle-click="onPrev"
+        id="data-carousel-prev"
+        :classes="'absolute top-0 left-0 z-30 flex h-full cursor-pointer items-center justify-center px-4'"
     >
-      <span
-        class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-font-dark bg-opacity-20 transition delay-75 duration-500 ease-in-out hover:bg-opacity-50 hover:text-primary"
-      >
-        <i class="fa-solid fa-chevron-left text-dark"></i>
-      </span>
-    </button>
-    <button
-      @click="onNext()"
-      id="data-carousel-next"
-      type="button"
-      class="absolute top-0 right-0 z-30 flex h-full cursor-pointer items-center justify-center px-4"
+      <template #icon>
+        <span
+            class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-font-dark bg-opacity-20 transition delay-75 duration-500 ease-in-out hover:bg-opacity-50 hover:text-primary"
+        >
+          <i class="fa-solid fa-chevron-left text-dark"></i>
+        </span>
+      </template>
+    </Button>
+    <Button
+        :handle-click="onNext"
+        id="data-carousel-next"
+        :classes="'absolute top-0 right-0 z-30 flex h-full cursor-pointer items-center justify-center px-4'"
     >
-      <span
-        class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-font-dark bg-opacity-20 transition delay-75 duration-500 ease-in-out hover:bg-opacity-50 hover:text-primary"
-      >
-        <i class="fa-solid fa-chevron-right text-dark"></i>
-      </span>
-    </button>
+      <template #icon>
+        <span
+            class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-font-dark bg-opacity-20 transition delay-75 duration-500 ease-in-out hover:bg-opacity-50 hover:text-primary"
+        >
+          <i class="fa-solid fa-chevron-right text-dark"></i>
+        </span>
+      </template>
+    </Button>
+    <span class="text-sm">&copy; https://github.com/duffn/dumb-password-rules</span>
   </div>
 </template>
 
 <script>
 
+import Button from '@/components/button/Button';
+
 export default {
 	components: {
-		/*Button*/
+		Button
 	},
 	data() {
 		return {
@@ -75,7 +79,7 @@ export default {
 	},
 	methods: {
 		populateImages() {
-			//webpack function to get all files in a specified folder
+			// webpack function to get all files in a specified folder
 			const context = require.context(
 				'@/assets/images/dumbPasswords/',
 				true,
@@ -85,7 +89,7 @@ export default {
 				.keys()
 				.forEach((key) =>
 					this.imageSources.push(
-						//populate imageSources array with image paths
+						// populate imageSources array with image paths
 						require(`@/assets/images/dumbPasswords/${key.replace('./', '')}`)
 					)
 				);
@@ -112,7 +116,6 @@ export default {
 	},
 	mounted() {
 		this.populateImages();
-
 	}
 };
 </script>
