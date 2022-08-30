5d258c638412aefce00f05f50a0a8bdc83073ffa
diana <diana.maria.schilling@gmail.com>
2022-08-30 22:46:08 +0200

    improve CrackingTimeCalculationView.vue

--- src/views/pages/CrackingTimeCalculationView.vue
+++ src/views/pages/CrackingTimeCalculationView.vue
@@ -3,11 +3,13 @@
     <template>
       <main class="tex2jax_process">
         <Paragraph
-          :paragraph="'Now that we know how many guesses an attacker needs to crack the password, we can derive the the time it would take.'"
+          :paragraph="'Now that we know how many guesses an attacker needs to crack the password, we can derive the time to crack $t$.'"
         />
         <Paragraph
-          :paragraph="'Modern GPUs can execute approximately $10$ Billion (<a href=\'https://en.wikipedia.org/wiki/Long_and_short_scales\' target=\'_blank\' class=\'underline\'>short scale</a>) guesses per second. If we take that into consideration we can easily predict the time to guess depending on a given entropy.'"
+          :paragraph="'Modern GPUs can execute approximately $10$ billion (<a href=\'https://en.wikipedia.org/wiki/Long_and_short_scales\' target=\'_blank\' class=\'underline\'>short scale</a>) guesses per second. If we take that into consideration, we can easily predict the time to guess depending on a given entropy.'"
         />
+        <Paragraph :paragraph="'Based on the current average cost to rent a GPU that achieves $10$ billion guesses per second ($30$ ct / h), we can then also predict the average overall cost $c$ depending on the entropy and the number of utilized GPUs.'"/>
+
         <div class="rounded bg-font-light p-10">
           <div class="mb-8">
             <label
@@ -42,11 +44,15 @@
             />
           </div>
           <div class="mb-3 mt-20">
-            <Paragraph :paragraph="getTimeToCrackFormula" class="text-xl sm:text-3xl" />
+            <Paragraph :paragraph="'<span class=\'text-xl sm:text-2xl inline-block mb-2\'>'+getTimeToCrackFormula+'</span> <span class=\'text-base sm:text-xl inline-block\'>'+getTimeToCrackResult+'</span>'"/>
+            <Paragraph :paragraph="'$c =\\text{ }'+ calculateAverageCosts() + '\\small{\\text{ \\$}}$'" class="text-base sm:text-xl"/>
           </div>
+          <span class="text-xs">M = 1 Million, B = 1 Billion, T = 1 Trillion, Q = 1 Quadrillion (according to the short scale)</span><br>
+          <span class="text-xs">Cracking times above 13,8B years are categorized as "longer than the universe exists".</span>
         </div>
+        <PowersOfTwoToPowersOfTenConverter/>
         <Paragraph
-          :paragraph="'With that in mind, let\'s look at the word <em>P@ssw0rd!</em>. With an entropy of $59$ bits it should take an attacker with a single GPU approximately $167$ days to crack. Sounds like a decent password, right?'"
+          :paragraph="'With that in mind, let\'s look at the word <em>P@ssw0rd!</em>. With an entropy of $58$ bits it should take an attacker with a single GPU approximately $167$ days to crack. Sounds like a decent password, right?'"
         />
         <Paragraph :paragraph="this.passwordEntropyString" />
         <Paragraph :paragraph="this.passwordGuessesString" />
@@ -65,21 +71,32 @@
 <script>
 import PageCard from '@/components/cards/PageCard';
 import Paragraph from '@/components/text/Paragraph';
+import PowersOfTwoToPowersOfTenConverter from '@/components/converter/PowersOfTwoToPowersOfTenConverter';
 import { mapState } from 'vuex';
 import { initializeMathJaxQueue } from '@/utils/initializeMathJaxQueue';
+
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * SECONDS_PER_MINUTE;
+const SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
+const SECONDS_PER_YEAR = SECONDS_PER_DAY * 365;
+const SECONDS_PER_MILLISECOND = 0.001;
+const YEARS_UNIVERSE_EXISTS = 13.8 * Math.pow(10, 10);
 
 export default {
 	components: {
 		PageCard,
-		Paragraph
+		Paragraph,
+		PowersOfTwoToPowersOfTenConverter
 	},
 	data() {
 		return {
+
 			passwordEntropyString:
         '$$H = log_{2}(95^9) = ' + this.calculateEntropy(95, 9) + '$$',
 			passwordGuessesString: '$$g = \\frac{95^9}{2} = 2^{59-1} = 2^{58}$$',
 			crackingTimeString:
-        '$$t = \\frac{2^{58}}{10 \\text{B}} = 167 \\text{ d}$$'
+        '$$t = \\frac{2^{58}}{10 \\text{B}} = 167 \\text{ d}$$',
+			averageCostPerGPUPerHour: .3
 		};
 	},
 	computed: {
@@ -90,10 +107,11 @@ export default {
         this.getPowerOfTwoFormula() +
         '}{\\class{text-highlight1}{' +
         this.numberOfGPUs +
-        '} \\cdot 10\\text{B}} = ' +
-        this.getTimeToCrack() +
-        '$'
+        '} \\cdot 10\\text{B}} = $'
 			);
+		},
+		getTimeToCrackResult(){
+			return '$\\text{ }' + this.getTimeToCrackFormatted() + '$';
 		}
 	},
 	methods: {
@@ -121,52 +139,83 @@ export default {
 			);
 			this.initializeMathJaxQueue(this);
 		},
-		getTimeToCrack() {
-			const time =
-        this.calculateGuessesNeeded() / this.getTotalGuessesPerSecond();
-			const secondsPerMinute = 60;
-			const secondsPerHour = secondsPerMinute * secondsPerMinute;
-			const secondsPerDay = secondsPerHour * 24;
-			const secondsPerYear = secondsPerDay * 365;
-			const secondsPerMillisecond = 0.001;
+		getTimeToCrack(){
+			return this.calculateGuessesNeeded() / this.getTotalGuessesPerSecond();
+		},
+		getTimeToCrackFormatted() {
+			const time = this.getTimeToCrack();
 
-			let res;
 
-			if (time > secondsPerYear) {
-				res = this.displayAsPowerOfTen(time / secondsPerYear) + '\\text{ y }';
+			if (time > SECONDS_PER_YEAR) {
+				const years = time / SECONDS_PER_YEAR;
+				if ( years > YEARS_UNIVERSE_EXISTS) {
+					return '\\text{longer than the universe exists}';
+				}
+
+				return (this.displayAsNumeral(years) + '\\text{ y }');
 			}
-			else if (time > secondsPerDay) {
-				res = Math.round(time / secondsPerDay) + '\\text{ d }';
+			if (time > SECONDS_PER_DAY) {
+				return (Math.round(time / SECONDS_PER_DAY) + '\\text{ d }');
 			}
-			else if (time > secondsPerHour) {
-				res = Math.round(time / secondsPerHour) + ' \\text{ h }';
+			if (time > SECONDS_PER_HOUR) {
+				return (Math.round(time / SECONDS_PER_HOUR) + ' \\text{ h }');
 			}
-			else if (time > secondsPerMinute) {
-				res = Math.round(time / secondsPerMinute) + ' \\text{ min }';
+			if (time > SECONDS_PER_MINUTE) {
+				return (Math.round(time / SECONDS_PER_MINUTE) + ' \\text{ min }');
 			}
-			else if (time < secondsPerMillisecond) {
-				res = ' \\text{< 1 ms}';
+			if (time < SECONDS_PER_MILLISECOND) {
+				return (' \\text{< 1 ms}');
 			}
-			else if (time < 1) {
-				res = Math.round(time / secondsPerMillisecond) + ' \\text{ ms }';
-			}
-			else {
-				res = Math.round(time) + '\\text{ s }';
+			if (time < 1) {
+				return (Math.round(time / SECONDS_PER_MILLISECOND) + ' \\text{ ms }');
 			}
 
-			return res;
+			return (Math.round(time) + '\\text{ s }');
+
+		},
+		displayAsNumeral(number){
+			if (number < 1e6){
+				return Math.round((number) * 100) / 100;
+			}
+			if (number < 1e9){
+				return (Math.round((number / 1e6) * 100) / 100) + '\\text{M}';
+			}
+			if (number < 1e12){
+				return (Math.round((number / 1e9) * 100) / 100) + '\\text{B}';
+			}
+			if (number < 1e15){
+				return (Math.round((number / 1e12) * 100) / 100) + '\\text{T}';
+			}
+			if (number < 1e18){
+				return (Math.round(number / 1e15) * 100 / 100) + '\\text{Q}';
+			}
+
+			return '> 1\\text{ quadrillion}';
 		},
 		displayAsPowerOfTen(number) {
-			if (number <= 100000) {
-				return Math.round(number);
+			if (number < 1000000) {
+
+				return Math.round((number) * 100) / 100;
+
 			}
 			const pot = Math.floor(Math.log10(number));
+			console.log(number);
+			console.log(pot);
 			const result = `10^{${ pot }}`;
 
 			return result;
 		},
 		calculateEntropy(range, length) {
 			return Math.round(Math.log2(Math.pow(range, length)) * 100) / 100;
+		},
+		calculateAverageCosts() {
+			let hours = Math.round(this.getTimeToCrack() / SECONDS_PER_HOUR);
+			if (hours < 1) {
+				hours = 1;
+			}
+			console.log(hours);
+
+			return this.displayAsNumeral(hours * this.averageCostPerGPUPerHour * this.numberOfGPUs);
 		}
 	},
 	mounted() {
