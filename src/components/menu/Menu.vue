37396ce3a792e980829a8054c06e2e135d61a262
diana <diana.maria.schilling@gmail.com>
2022-08-30 18:57:59 +0200

    fix menu layout

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div
-    class="pl-15 md:py-20 overflow-y-auto flex flex-col min-w-[30%] fixed h-screen lg:h-auto lg:relative max-w-[30%] basis-1/2 bg-secondary p-5 lg:p-10 text-left font-prozaLibre text-font-light text-sm xl:text-base"
+    class="pl-15 md:py-20 overflow-y-auto flex flex-col md:min-w-[30%] fixed h-screen lg:h-auto lg:relative md:max-w-[30%] basis-1/2 bg-secondary p-5 lg:p-10 text-left font-prozaLibre text-font-light text-sm xl:text-base"
   >
     <div class="flex flex-col justify-center m-auto">
       <ul class="m-auto flex h-1/2 flex-col justify-evenly">
