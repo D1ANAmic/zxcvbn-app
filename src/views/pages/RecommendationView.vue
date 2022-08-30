cbd0e1ea9e189b1dcf17c9026d9ca8e638e3dabd
diana <diana.maria.schilling@gmail.com>
2022-08-30 18:58:18 +0200

    fix RecommendationView.vue

--- src/views/pages/RecommendationView.vue
+++ src/views/pages/RecommendationView.vue
@@ -16,7 +16,7 @@
           :paragraph="'Despite Zxcvbn providing the tools to compose a strong password, the endless amount of accounts that are now a fixture in our everyday lives pose a challenge to the creative mind and memory.'"
         />
         <Paragraph
-          :paragraph="'That\'s where password managers come into play. Not only do most of them provide you with strong passwords out of the box but since they serve as a safe, you only need to remember the main password. To prevent possible leaks, a local or self hosted password manager is a good solution. If you have the chance, pair it with multi-factor authentication. And as for the main password: Use Zxcvbn to compose a strong one.'"
+          :paragraph="'That\'s where password managers come into play. Not only do most of them provide you with strong passwords out of the box but since they serve as a safe, you only need to remember the main password. To prevent possible leaks, a local or self hosted password manager is a good solution. If you have the chance, pair it with multi-factor authentication. And as for the main password: Use Zxcvbn concepts to compose a strong one.'"
         />
       </main>
     </template>
