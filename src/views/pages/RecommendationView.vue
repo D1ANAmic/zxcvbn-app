29253dc8a71182fb82b815076e7931c560696a41
diana <diana.maria.schilling@gmail.com>
2022-08-30 08:52:44 +0200

    refactor RecommendationView.vue

--- src/views/pages/RecommendationView.vue
+++ src/views/pages/RecommendationView.vue
@@ -2,11 +2,22 @@
   <PageCard :header="'How to keep your accounts secure'">
     <template>
       <main>
-<Paragraph :paragraph="'&quotSo are there any recommendations to ensure password security?&quot, you might ask. Well, first of all don\'t submit your passwords to random apps disguised as educational programs.'"></Paragraph>
-        <img src="../../assets/images/horseScale/horse_smirk.png" class="inline max-w-[100px]"/>
-        <Paragraph :paragraph="'Just kidding. Although it is crucial to be cautious about who you trust with your password. Treacherous attackers lurk everywhere. '"></Paragraph>
-        <Paragraph :paragraph="'Despite Zxcvbn providing the tools to compose a strong password, the endless amount of accounts that are now a fixture in our everyday lives pose a challenge to the creative mind and memory.'"></Paragraph>
-        <Paragraph :paragraph="'That\'s where password managers come into play. Not only do most of them provide you with strong passwords out of the box but since they serve as a safe, you only need to remember the main password to open the safe. To avoid any possible breaches, a local or self hosted password manager is a good solution. If you have the chance, pair it with multi-factor authentication. And as for the main password: Use Zxcvbn to compose a strong one'"></Paragraph>
+        <Paragraph
+          :paragraph="'&quotSo are there any recommendations to ensure password security?&quot, you might ask. Well, first of all don\'t submit your passwords to random apps disguised as educational programs.'"
+        />
+        <img
+          src="../../assets/images/horseScale/horse_smirk.png"
+          class="inline max-w-[100px]"
+        />
+        <Paragraph
+          :paragraph="'Just kidding. Although it is crucial to be cautious about who you trust with your password. Treacherous attackers lurk everywhere. '"
+        />
+        <Paragraph
+          :paragraph="'Despite Zxcvbn providing the tools to compose a strong password, the endless amount of accounts that are now a fixture in our everyday lives pose a challenge to the creative mind and memory.'"
+        />
+        <Paragraph
+          :paragraph="'That\'s where password managers come into play. Not only do most of them provide you with strong passwords out of the box but since they serve as a safe, you only need to remember the main password. To prevent possible leaks, a local or self hosted password manager is a good solution. If you have the chance, pair it with multi-factor authentication. And as for the main password: Use Zxcvbn to compose a strong one.'"
+        />
       </main>
     </template>
   </PageCard>
@@ -23,4 +34,3 @@ export default {
 	}
 };
 </script>
-
